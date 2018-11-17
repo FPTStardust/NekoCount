cacadb0b92efe2c174fde6f57f34ba83213147ab
Spectre Lee <33302106+FPTStardust@users.noreply.github.com>
2018-11-17 16:08:13 +0800

    解决中午开始切换日期的问题
    增加了想关注释。

--- NekoCount/js/main.js
+++ NekoCount/js/main.js
@@ -1,13 +1,14 @@
 
 	
-	var memorial = new Date("2018-07-13");
+	var memorial = new Date("2018-07-14");
+	//把这个日期改成相应的纪念日即可。
 	var systime = new Date();
-	var dif = parseInt(Math.abs(systime - memorial) / 1000 / 60 / 60 / 24);
+	var dif = parseInt(Math.abs(systime - memorial+36*60*60*1000) / 1000 / 60 / 60 / 24);
+	//由于new Date的日期是从中午十二点开始的，外加程序里第一天会被算成第0天，所以需要在system time上加上36小时。
 	var bnrDate = dif.toString(2).split("");
 	
 $(document).ready(function(){
 	"use strict";
-	//$("body").css("width", window.screen.width * window.devicePixelRatio);
 	$("body").css("width", window.innerWidth);
 	$("body").css("height", window.innerHeight);
 	$(window).resize(function () {
@@ -25,7 +26,7 @@ $(document).ready(function(){
 			if(bnrDate.hasOwnProperty(x)){
 			if(bnrDate[x]==="1") {$("#neko").append('<img height="35px" src="img/ichi.png">');}
 			if(bnrDate[x]==="0") {$("#neko").append('<img height="35px" src="img/zero.gif">');}
-			}
+			}//这里是吧01串转化为滑稽和纠结的表情，当然可以单纯的换成喵咪之类的汉字，或者其他各种支持的元素，直接写HTML代码即可。
 		}
   	});
 });
